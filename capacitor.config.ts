20c154f74ee5d707552a1b1bc814fcd8981db083
prodygey <everyoneispoor00@gmail.com>
2026-09-14 05:13:28 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mga-sining-sa-museo',
+  webDir: 'dist'
+};
+
+export default config;
